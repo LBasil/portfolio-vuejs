b30f34bab532e407421777bece7ea4de616a01ab
Basil L'hôte <blhote@misyl.net>
2023-06-29 16:00:04 +0200

    Fin 'projets'

--- src/components/ProjetsComponent.vue
+++ src/components/ProjetsComponent.vue
@@ -5,7 +5,13 @@
       <div class="row">
         <div v-for="projet in projets" :key="projet.id" class="col-md-6">
           <div class="card mb-4">
-            <img :src="projet.image" class="card-img-top" :alt="projet.titre" />
+            <div class="project-image">
+              <img
+                :src="projet.image"
+                class="card-img-top"
+                :alt="projet.titre"
+              />
+            </div>
             <div class="card-body">
               <h5 class="card-title">{{ projet.titre }}</h5>
               <p class="card-text">{{ projet.description }}</p>
@@ -39,6 +45,13 @@ export default {
           image: "chemin_vers_image_projet_2",
           lien: "#",
         },
+        {
+          id: 3,
+          titre: "Projet 3",
+          description: "Nullam condimentum consequat ex sed elementum.",
+          image: "chemin_vers_image_projet_2",
+          lien: "#",
+        },
         // Ajoutez d'autres projets ici
       ],
     };
@@ -47,5 +60,62 @@ export default {
 </script>
 
 <style scoped>
-/* Ajoutez des styles personnalisés ici */
+.project-image {
+  position: relative;
+  overflow: hidden;
+  height: 300px;
+}
+
+.project-image::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.project-image:hover::before {
+  opacity: 1;
+}
+
+.card {
+  position: relative;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s, box-shadow 0.3s;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2);
+}
+
+.card-body {
+  padding: 20px;
+}
+
+.card-title {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.card-text {
+  margin-bottom: 15px;
+}
+
+.btn-primary {
+  background-color: #007bff;
+  border-color: #007bff;
+  transition: background-color 0.3s, border-color 0.3s;
+}
+
+.btn-primary:hover {
+  background-color: #0056b3;
+  border-color: #0056b3;
+}
 </style>
